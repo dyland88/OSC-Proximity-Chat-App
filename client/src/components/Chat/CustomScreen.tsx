508bdef7734a42cb151e8b8df3d965fc57fef281
Dylan <dylan.coben@gmail.com>
2024-02-01 18:30:10 -0500

    Added Chat Elements to Custom Screen

--- client/src/components/Chat/CustomScreen.tsx
+++ client/src/components/Chat/CustomScreen.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, SafeAreaView, Switch} from 'react-native'; // And all other imports
-
-// Any logic should be right above the component
+import SafeAreaWrapper from '../Common/SafeAreaWrapper'; // Import the SafeAreaWrapper component
+import { ChatSendButton } from '../Common/CustomButtons'; // Import the ChatSendButton component
+import { ChatInput } from '../Common/CustomInputs'; // Import the ChatInput component
 
 const CustomScreen : React.FC = () => {
     const [isEnabled, setIsEnabled] = useState(false);
@@ -14,30 +15,39 @@ const CustomScreen : React.FC = () => {
 
 
     return (
-        <SafeAreaView style = {styles.wrapper}>
+        <SafeAreaWrapper>
             <View style={styles.container}>
                 <Text style={styles.hellotext}>Hello World</Text>
                 <Text>More text</Text>
+                
             </View>
             <Switch style={styles.switch}
                 onValueChange={toggleSwitch}
                 value={isEnabled}
             />
-        </SafeAreaView>
+
+            <View style={styles.chatElements}>
+                <ChatInput />
+                <ChatSendButton />
+            </View>
+            
+        </SafeAreaWrapper>
     );
 };
 
 const styles = StyleSheet.create({
     // Styles
+    chatElements: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
+    },
+
     container: {
         backgroundColor: 'pink',
     },
 
-    wrapper: {
-        backgroundColor: 'lightblue',
-        flex: 1,
-        alignItems: 'center'
-    },
 
     hellotext: {
         fontSize: 24,
